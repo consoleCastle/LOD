919f76777dc99c52d8445dabc8fefbe3d368345c
Krista Singleton <krista.singleton@intimetec.com>
2021-07-27 13:50:32 -0600

    Created game endType class and added psuedo code for PlayGame class

---
+++ LOD/Classes/EndType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LOD.Classes
+{
+    class EndType
+    {
+        //Need a bool that checks if this is a GameOver type or not (Will determine if fancy console GAMEOVER or SUCCESS message is displayed
+        //Need a functionality that receives a message from the game that describes to the player why the game ended- IE: "You have been impaled on spikes" OR "You opened the great stone door and escaped!"
+        //Need to pass this information to the End() function in PlayGame
+    }
+}
--- LOD/Classes/PlayGame.cs
+++ LOD/Classes/PlayGame.cs
@@ -11,20 +11,56 @@ namespace LOD.Classes
         GameData data = new GameData();
         public void Start()
         {
+            Console.WriteLine(data.lineSpacer);
             Console.WriteLine(data.TitleArt);
+            Console.WriteLine(data.lineSpacer);
+            // Add timer / loading screen
+            // Print setting and character exposition
+            // Start room sequence (starts at top of mountain, player works through rooms)
+            // if character triggers game ending
+              // Run End(with passed endtype class)
+
+        }
+        public void ThanksForPlaying()
+        {
+            //Print thank you message and credits
+            //Close console app
+        }
+        public void Reset()
+        {
+            //Run reset player flags
+            //Run reset player location (?)
+        }
+        public void End(EndType endType)
+        {
+            //If endtype is equal to gameover
+            //Console.WriteLine(EndType.Message);
+            //Thread.Sleep(1000);
+            //Console.WriteLine(data.Gameover);
+
+            //If endtype is equal to success
+            //Console.WriteLine(EndType.Message);
+            //Thread.Sleep(1000);
+            //Console.WriteLine(data.Success);
+
+            //Run Reset()
             //Thread.Sleep(5000);
+            //Ask player if they would like to play again
+            //If "yes" run Start()
+            //If "no" run ThanksForPlaying()
+        }
+        public void RunLoadingAnimation(int seconds)
+        {
+            /*
+            Thread.Sleep(5000);
+            
             LoadingAnimation loading = new LoadingAnimation();
             loading.Delay = 500;
             while (true)
             {
                 loading.Run("Loading", sequenceCode: 1);
             }
-        }
-        public void GameOver()
-        {
-            Console.WriteLine("You have died.");
-            Thread.Sleep(1000);
-            Console.WriteLine(data.Gameover);
+            */
         }
     }
 }
