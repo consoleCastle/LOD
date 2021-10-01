101c6c422dcf26ef416e81de6c51e67b597dba41
agiannell-ts <anthonygiannell@truckstop.com>
2021-10-01 13:02:43 -0600

    feat: menu work

--- LOD/Utils/GameData.cs
+++ LOD/Utils/GameData.cs
@@ -8,31 +8,37 @@ namespace LOD.Classes
 {
     class GameData
     {
-        public Room CurrentRoom { get; set; } 
+        public Room CurrentRoom { get; set; }
+        private string RoomOneDescription = "This is the default description before you flip a switch in room 1. In room 2, you die. Instructions: Type the number of your choice and hit enter.";
+        private string[] RoomOneOptions = { "Go to Room 1", "Go To Room 2" };
+        private string RoomTwoDescription = "There is a switch in this room. Neato! Type 'a' to flip it. (a is for Action)\n1. Go back to the starting room\nA. Flip the switch!";
+        private string[] RoomTwoOptions = { "Go back to the starting room", "Flip the switch!" };
+        private string RoomThreeDescription = "Oh no! You died!";
         public GameData()
         {
             CurrentRoom = SetUpRooms();
         }
-<<<<<<< HEAD:LOD/Classes/GameData.cs
-        public Room SetUpRooms()
+
+        public Room SetUpRooms() 
         {
-            Room test_starting_room = new Room("test_starting_room", "This is the default description before you flip a switch in room 1. In room 2, you die. Instructions: Type the number of your choice and hit enter.\n1. Go to Room 1\n2. Go to Room 2");
-            Room test_room_1 = new Room("test_room_1", "There is a switch in this room. Neato! Type 'a' to flip it. (a is for Action)\n1. Go back to the starting room\nA. Flip the switch!");
-            Room test_room_2 = new Room("test_room_2", "Oh no! You died!");
+            Room test_starting_room = new Room("test_starting_room", RoomOneDescription, RoomOneOptions);
+            Room test_room_1 = new Room("test_room_1", RoomTwoDescription, RoomTwoOptions);
+            Room test_room_2 = new Room("test_room_2", RoomThreeDescription);
 
 
             test_starting_room.Choices.Add("1", test_room_1);
             test_starting_room.Choices.Add("2", test_room_2);
+            test_starting_room.ShowMenu(test_starting_room.Options);
 
             test_room_1.Choices.Add("1", test_starting_room);
             return test_starting_room;
         }
-        public void CheckStatement (PlayerFlags playerFlags, string userCommand)
+        public void CheckStatement(PlayerFlags playerFlags, string userCommand)
         {
-            switch(userCommand)
+            switch (userCommand)
             {
                 case "a":
-                    if(CurrentRoom.Name == "test_room_1")
+                    if (CurrentRoom.Name == "test_room_1")
                     {
                         playerFlags.Three_Stones_Collected = true;
                     }
@@ -41,7 +47,7 @@ namespace LOD.Classes
                     //TODO make the menu come up
                     break;
                 default:
-                    if(!CurrentRoom.Choices.ContainsKey(userCommand))
+                    if (!CurrentRoom.Choices.ContainsKey(userCommand))
                     {
                         Console.WriteLine("That is not a valid choice");
                         break;
@@ -67,12 +73,6 @@ namespace LOD.Classes
             }
             return false;
         }
-            return test_starting_room;
-        }
-=======
-
-        public string Exposition { get; } = "A bright flash of light blinds your eyes as the world around you dissolves. After a moment of disorientation you find yourself standing in a small courtyard atop a calm mountain. What happened? How did you get here? How do you get home? The answers are hidden in the world around you.";
-        //newline
-
     }
 }
+            
