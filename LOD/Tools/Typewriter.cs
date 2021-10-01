dc517ee9dfe908bfefddd6f649782afb43d01222
Krista Singleton <krista.singleton@intimetec.com>
2021-10-01 15:08:50 -0600

    Testing Merge conflicts

--- LOD/Tools/Typewriter.cs
+++ LOD/Tools/Typewriter.cs
@@ -16,6 +16,7 @@ namespace LOD.Tools
         public void Type(string message, int speed)
         {
             TypeWithLineBreaks(0, message, speed);
+            //Testing Merge conflicts 2
         }
 
         public void GiveMeSpace()
