e2c77028cda9f33d0ac953e6db43d38090675738
agiannell-ts <anthonygiannell@truckstop.com>
2021-10-08 15:24:41 -0600

    added menu to rooms, added common menu to each room

---
+++ LOD/Tools/CommonMenu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LOD.Utils;
+
+namespace LOD.Tools
+{
+    class CommonMenu
+    {
+        private static string prompt = "Welcome to the Menu";
+        private static string[] options = { "Show Map", "Close Menu" };
+        public static void ShowCommonMenu()
+        {
+            Menu commonMenu = new Menu(prompt, options);
+            int selectedIndex = commonMenu.Run();
+
+            switch(selectedIndex)
+            {
+                case 0:
+                    ShowMap();
+                    break;
+                case 1:
+                    Console.Clear();
+                    break;
+            }
+        }
+
+        private static void ShowMap()
+        {
+            AsciiArt art = new AsciiArt();
+            Console.WriteLine(art.Map);
+            Console.WriteLine("Press any key to return to the menu...");
+            Console.ReadKey(true);
+            ShowCommonMenu();
+        }
+    }
+
+}
